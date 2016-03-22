ce318aa4e4c5d803d4784e607ad1514426053a36
Drew Haas <drew@bonfirered.com>
2016-03-22 18:51:55 -0400

    everything

--- index.php
+++ index.php
@@ -2,16 +2,22 @@
 <html lang="en">
   <?php include('includes/head.inc'); ?>
   <body class="home">
-  <div class="container">
-    <div class="logo"></div>
-    <h1>L&R Technologies</h1>
-    <h2>Lorem Ipsum</h2>
-    <h2>We're Coming Soon</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus elementum lacinia. Aliquam vel lacus turpis. Donec in pellentesque lectus, eget bibendum lacus. Sed laoreet hendrerit libero non accumsan. Suspendisse feugiat enim ut arcu fermentum, vitae placerat mauris malesuada. Donec mollis facilisis maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-    <div class="cta">
-      <a href="#">Contact US</a>
+  <div id="colorchanger" class="bg-container">
+    <div class="container">
+      <div class="logo">
+        <?php include('includes/logo.inc'); ?>
+      </div>
+      <h1>L&R Technologies</h1>
+      <h2>Lorem Ipsum</h2>
+      <h2>We're Coming Soon</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus elementum lacinia. Aliquam vel lacus turpis. Donec in pellentesque lectus, eget bibendum lacus. Sed laoreet hendrerit libero non accumsan. Suspendisse feugiat enim ut arcu fermentum.</p>
+      <div class="cta-container">
+        <a href="#">Contact US</a>
+        <div class="bg-color"></div>
+      </div>
     </div>
   </div>
+  
     <?php //include('includes/footer.inc'); ?>
     <script type="text/javascript" src="/assets/js/home.js"></script>
   </body>
