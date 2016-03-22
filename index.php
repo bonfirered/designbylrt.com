78024ca2a32dcf40f7b7dcf9b55f97172d155653
Drew Haas <drew@bonfirered.com>
2016-03-22 16:05:46 -0400

    updated html and npm file

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 <html lang="en">
   <?php include('includes/head.inc'); ?>
   <body class="home">
-    <div class="full-screen">
-      <?php //include('includes/nav.inc'); ?>
-      <div class="clock">
-        <h2><!-- 12:46:34 --><?php echo gmdate ("h:i:s");?></h2>
-      </div>
+  <div class="container">
+    <div class="logo"></div>
+    <h1>L&R Technologies</h1>
+    <h2>Lorem Ipsum</h2>
+    <h2>We're Coming Soon</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus elementum lacinia. Aliquam vel lacus turpis. Donec in pellentesque lectus, eget bibendum lacus. Sed laoreet hendrerit libero non accumsan. Suspendisse feugiat enim ut arcu fermentum, vitae placerat mauris malesuada. Donec mollis facilisis maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <div class="cta">
+      <a href="#">Contact US</a>
     </div>
+  </div>
     <?php //include('includes/footer.inc'); ?>
     <script type="text/javascript" src="/assets/js/home.js"></script>
   </body>
