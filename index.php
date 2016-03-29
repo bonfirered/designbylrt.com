1ac36bd458f773daa733e480be4e958064e1c1dc
Drew Haas <drew@bonfirered.com>
2016-03-29 10:12:23 -0400

    updated button styling and font smoothing

--- index.php
+++ index.php
@@ -2,26 +2,24 @@
 <html lang="en">
   <?php include('includes/head.inc'); ?>
   <body class="home">
-  <div id="colorchanger" class="bg-container">
-    <div class="container">
-      <div class="logo">
-        <?php include('includes/logo.inc'); ?>
-      </div>
-      <div class="logo-text">
-        <?php include('includes/logo-text.inc'); ?>
-      </div>
-      <h1>L&R Technologies</h1>
-      <h2>The Ultimate<br>in CATIA Design</h2>
-      <div class="divider back-to-it"></div>
-      <p>L&R Technologies is the premier technical design company specializing in CATIA V5. Backed by 30 years of combined experience, L&R Technologies helps to bridge the gap between manufacturing and engineering with user-centric design, streamlined methodology, and unmatched results that speak for themselves.</p>
-      <div class="cta-container">
-        <a href="#">Contact US</a>
-        <div class="bg-color"></div>
+    <div id="colorchanger" class="bg-container">
+      <div class="container">
+        <div class="logo">
+          <?php include('includes/logo.inc'); ?>
+        </div>
+        <div class="logo-text">
+          <?php include('includes/logo-text.inc'); ?>
+        </div>
+        <h1>L&R Technologies</h1>
+        <h2>The Ultimate<br>in CATIA Design</h2>
+        <div class="divider back-to-it"></div>
+        <p>L&R Technologies is the premier technical design company specializing in CATIA V5. Backed by 30 years of combined experience, L&R Technologies helps to bridge the gap between manufacturing and engineering with user-centric design, streamlined methodology, and unmatched results that speak for themselves.</p>
+        <div class="cta-container">
+          <a href="mailto:admin@designbylrt.com" target="_top">Contact US</a>
+          <div class="bg-color"></div>
+        </div>
       </div>
     </div>
-  </div>
-  
-    <?php //include('includes/footer.inc'); ?>
-    <script type="text/javascript" src="/assets/js/home.js"></script>
+  <script type="text/javascript" src="/assets/js/home.js"></script>
   </body>
 </html>
